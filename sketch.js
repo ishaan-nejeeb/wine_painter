67421ba696383f6bcb0fd20b0913a237c3dfa52e
ishaan-nejeeb <nejeeb.ishaan@gmail.com>
2022-04-01 04:12:17 +0530

    inital

---
+++ sketch.js
@@ -0,0 +1,137 @@
+let table;
+let maxCitric,
+  minCitric,
+  maxDensity,
+  minDensity,
+  maxPh,
+  minPh,
+  maxQuality,
+  minQuality,
+  minAlcohol,
+  maxAlcohol;
+let index = 190;
+let res;
+let nosieVal;
+let bg = "#000000";
+let xStep, yStep;
+let quality, density, alcohol, ph;
+let num;
+let iteration = 0;
+let maxIteration = 50;
+let maxNum = 70;
+let minNum = 20;
+let minRes = 5;
+let maxRes = 50;
+let resConst = 5;
+let hu;
+let minHu = 0;
+let maxHu = 250;
+
+let walkers = [];
+
+function preload() {
+  table = loadTable("WineQT.csv", "csv", "header");
+}
+
+function setup() {
+  frameRate(10);
+  createCanvas(1000, 1000);
+  background(bg);
+  maxCitric = max(table.getColumn("citric acid"));
+  minCitric = min(table.getColumn("citric acid"));
+  maxDensity = max(table.getColumn("density"));
+  minDensity = min(table.getColumn("density"));
+  maxPh = max(table.getColumn("pH"));
+  minPh = min(table.getColumn("pH"));
+  maxQuality = max(table.getColumn("quality"));
+  minQuality = min(table.getColumn("quality"));
+  maxAlcohol = max(table.getColumn("alcohol"));
+  minAlcohol = min(table.getColumn("alcohol"));
+//   // console.log(table);
+
+  quality = table.getNum(index, "quality");
+//   res = map(quality,minQuality,maxQuality,maxRes,minRes);
+//   let tempRes = 210/quality;
+//   res = map(tempRes,21,210,5,50);  
+  ph = table.getNum(index, "pH");
+  hu = map(ph,minPh,maxPh,minHu, maxHu);
+  res = (10-(quality-1))*resConst;
+  alcohol = table.getNum(index, "alcohol");
+  for (let i = 0; i < maxNum; i++) {
+    walkers[i] = new Walker(random(width), random(height),hu, alcohol, maxAlcohol, minAlcohol, res);
+	console.log(quality);
+	console.log(res);
+  }
+}
+
+function draw() {
+//   background(0,10);
+  //fill(255);
+  quality = table.getNum(index, "quality");
+  density = table.getNum(index, "density");
+  alcohol = table.getNum(index, "alcohol");
+//   console.log(alcohol);
+//   res = map(quality,minQuality,maxQuality,maxRes,minRes);
+//   res = 210/quality*5;
+//   let tempRes = 210/quality;
+//   res = map(tempRes,21,210,5,50);
+  hu = map(ph,minPh,maxPh,minHu, maxHu);
+  res = (10-(quality-1))*resConst;
+  res = (10-(quality-1))*resConst;
+  xStep = width / res;
+  yStep = height / res;
+  num = map(density, minDensity, maxDensity, minNum, maxNum);
+
+  
+  for (let i = 0; i < num; i++) {
+	walkers[i].update();
+	walkers[i].display();
+	
+    
+  }
+
+  
+//   fill(255);
+//   circle(500,500,50);
+  iteration++;
+  if (iteration > maxIteration) {
+    noLoop();
+  }
+//   noLoop();
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    index++;
+    if (index > 1143) {
+      index = 0;
+    }
+  } else if (keyCode === RIGHT_ARROW) {
+    index--;
+    if (index < 0) {
+      index = 1143;
+    }
+  } 
+	quality = table.getNum(index, "quality");
+	density = table.getNum(index, "density");
+	alcohol = table.getNum(index, "alcohol");
+	hu = map(ph,minPh,maxPh,minHu, maxHu);
+    res = (10-(quality-1))*resConst;
+	res = (10-(quality-1))*resConst;
+	num = map(density, minDensity, maxDensity, minNum, maxNum);
+	for (let i = 0; i < num; i++) {
+		walkers[i].updateVal(
+		random(width),
+		random(height),
+		hu,
+		alcohol,
+		maxAlcohol,
+		minAlcohol,
+		res
+		);
+	  }
+	  
+  background(bg);
+  iteration = 0;
+  loop();
+}
---
+++ walker.js
@@ -0,0 +1,127 @@
+class Walker {
+    constructor(x, y,hu, alcohol, maxA, minA,res) {
+      this.hu = hu;
+      this.res = res
+      this.noiseX = x;
+      this.noiseY = y;
+      this.noiseZ = 0;
+    //   this.x = map(noise(this.noiseX),0 ,1, 0, width);
+    //   this.y = map(noise(this.noiseY),0, 1, 0, height);
+      this.x = x;
+      this.y = y;
+      this.noiseIncrement = map(alcohol, minA, maxA, 0.01, 0.09);
+    //   this.noiseIncrement = 0.01;
+      this.pos = createVector(this.x, this.y);
+      this.prev = this.pos.copy();
+      this.angle = 0;
+    //   this.Xnoise;
+    //   this.Ynoise;
+    }
+  
+    updateVal(x, y, hu, alcohol, maxA, minA,res) {
+      this.hu = hu;
+      this.res =res
+      this.noiseX = x;
+      this.noiseY = y;
+      this.noiseZ = 0;
+      this.noiseIncrement = map(alcohol, minA, maxA, 0.001, 0.1);
+      this.pos.x = x;
+      this.pos.y = y;
+      this.prev = this.pos;
+    }
+  
+    display() {
+      strokeWeight(2);
+      stroke(255);
+      colorMode(HSL);
+      stroke(this.hu,66,53);
+        
+    //   push();
+    //   translate(this.prev.x, this.prev.y); 
+    //   rotate(this.angle); 
+      beginShape();
+      vertex(this.prev.x, this.prev.y);
+    //   console.log(this.prev);
+      vertex(this.pos.x, this.pos.y);
+    //   console.log(this.pos);
+      endShape();
+    //   line(this.prev.x, this.prev.y, this.pos.x, this.pos.y);
+    //   fill(255);
+    //   circle(this.pos.x, this.pos.y, 5);
+    //   pop();
+      this.prev.x = this.pos.x;
+      this.prev.y = this.pos.y;
+      
+    //   fill(255);
+    //   circle(this.pos.x, this.pos.y, 5);
+    //   console.log(this.pos);
+    }
+  
+    update() {
+      this.angle =
+        noise(this.pos.x, this.pos.y, this.noiseZ) * TWO_PI;// * noiseStrength; //0-2PI\
+      console.log(this.angle);  
+    //   this.Xnoise = noise(this.noiseX);
+    //   this.Ynoise = noise(this.noiseY);
+      this.Xnoise = cos(this.angle)*this.res;
+    //   console.log(cos(this.angle));
+    //   console.log(cos(this.angle));
+      console.log(this.res);
+    //   console.log(cos(this.angle)*this.res)
+    //   console.log(this.Xnoise);
+      this.Ynoise = sin(this.angle)*this.res;
+    //   console.log(sin(this.angle));
+    //   console.log(this.Ynoise);
+      this.Xnoise = this.pos.x+this.Xnoise;
+      this.Ynoise = this.pos.y+this.Ynoise;
+    //   this.Xnoise = map(this.Xnoise, 0, 1, 0, width);
+    //   this.Ynoise = map(this.Ynoise, 0, 1, 0, height);
+      this.step = createVector(this.Xnoise, this.Ynoise);
+    //   console.log(this.step);
+      
+    //   console.log(this.pos);
+    //   console.log(this.pos);
+      this.pos = this.step;
+    //   console.log(this.pos);
+    //   console.log(this.prev);
+    //   console.log(this.step);
+    //   this.pos.x=this.pos.x+Xnoise;
+    //   this.pos.y=this.pos.y+Ynoise;
+    //   console.log(this.pos);
+    //   this.pos.x = Xnoise;
+    
+    //   this.pos.y = Ynoise;
+      // console.log(this.step);
+      //this.pos.add(this.step);
+      //console.log(this.pos);
+  
+    //   if (this.pos.x < 0) {
+    //     this.pos.x = width;
+    //     this.prev.x = width;
+    //   } 
+    //   if (this.pos.x > width) {
+    //     this.pos.x = 0;
+    //     this.prev.x = 0;
+    //   }
+    //   if (this.pos.y < 0) {
+    //     this.pos.y = height;
+    //     this.prev.y = height;
+    //   }
+    //   if (this.pos.y > height) {
+    //     this.pos.x = 0;
+    //     this.prev.y = 0;
+    //   }
+
+      if(this.pos.x < 0 || this.pos.x > width || this.pos.y < 0 || this.pos.y > height){
+          this.pos.x = random(width);
+          this.pos.y = random(height);
+          this.prev.x = this.pos.x;
+          this.prev.y = this.pos.y;
+      }     
+   
+      this.noiseZ += this.noiseIncrement;
+    //   this.noiseX+=this.noiseIncrement;
+    //   this.noiseY+=this.noiseIncrement;
+    }
+  }
+  
